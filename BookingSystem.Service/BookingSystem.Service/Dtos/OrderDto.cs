a46c63584f762ec0ea0cde4474369c393955dc82
Kamil Kurzeja <asdqw@gmail.com>
2022-12-11 21:52:48 +0100

    emil fix

--- BookingSystem.Service/BookingSystem.Service/Dtos/OrderDto.cs
+++ BookingSystem.Service/BookingSystem.Service/Dtos/OrderDto.cs
@@ -4,6 +4,8 @@ namespace BookingSystem.Service.Dtos
 {
     public class OrderDto
     {
+        public bool IsEnd { get; set; }
+        public bool IsAccepted { get; set; }
         public int OrderId { get; set; }
         public string Adress { get; set; }
         public int? UserId { get; set; }
