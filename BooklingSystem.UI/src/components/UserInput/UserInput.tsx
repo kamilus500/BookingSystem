40268aa20e9654cf5a6e543f59ac884cc1c81976
ZrodoZrodo <112622013+ZrodoZrodo@users.noreply.github.com>
2022-12-15 18:35:13 +0100

    Update UserInput.tsx

--- BooklingSystem.UI/src/components/UserInput/UserInput.tsx
+++ BooklingSystem.UI/src/components/UserInput/UserInput.tsx
@@ -54,13 +54,12 @@ const UserInput: React.FC<{
   const { address, user } = orderState;
   const { firstName, lastName,email } = user;
   const { street, buildingNumber, city, zipCode } = address;
-
   const disableButton =
-    /^[A-Za-z]$/g.test(firstName) &&
-      /^[A-Za-z]$/g.test(lastName) &&
+    /^[A-Za-z]+$/g.test(firstName) &&
+      /^[A-Za-z]+$/g.test(lastName) &&
     street &&
       buildingNumber &&
-      /^[A-Za-z]$/g.test(city) &&
+      /^[A-Za-z]+$/g.test(city) &&
     /^[\d]{2}-[\d]{3}$/g.test(zipCode)&&
       /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/g.test(email);
 
