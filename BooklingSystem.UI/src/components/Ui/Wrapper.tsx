05192439b5f31cc82777e83266d337f9fb4e40fc
Damian Puś <pusiek99@interia.pl>
2022-12-15 19:57:49 +0100

    Coś tam

--- BooklingSystem.UI/src/components/Ui/Wrapper.tsx
+++ BooklingSystem.UI/src/components/Ui/Wrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col gap-4 items-center h-screen justify-center">
+    <div className="flex flex-col gap-4 my-10 items-center justify-center">
       {children}
     </div>
   );
